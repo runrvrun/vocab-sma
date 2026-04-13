722e38b5776ce7e67a375711350dd87a44754747
runrvrun <runrvrun@gmail.com>
2026-04-13 08:49:21 +0700

    seed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,14 @@
-import { PrismaClient } from "@/generated/prisma"
+import { PrismaClient } from "@/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL,
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL ?? "" }),
   })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+export default prisma
